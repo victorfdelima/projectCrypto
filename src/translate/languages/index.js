f0a2aca0065f4955f4f53e19d2d35897c6203ec5
pitluma <110204252+pitluma@users.noreply.github.com>
2022-09-30 09:59:31 -0300

    Delete src directory

--- src/translate/languages/index.js
+++
@@ -1,9 +0,0 @@
-import { messages as portugueseMessages } from './pt';
-import { messages as englishMessages } from './en';
-import { messages as spanishMessages } from './es';
-
-export const messages = {
-  ...portugueseMessages,
-  ...englishMessages,
-  ...spanishMessages,
-};
--- src/utils/formatCurrency.js
+++
@@ -1,9 +0,0 @@
-export function formatCurrency(value = 0, removePrefix = false) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 1,
-  })
-    .format(value)
-    .replace('$', removePrefix ? '' : '$');
-}
